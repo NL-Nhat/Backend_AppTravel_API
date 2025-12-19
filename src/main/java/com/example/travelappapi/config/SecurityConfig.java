c0abb1e664aa2c419ad216e2cbc382b43ed617a5
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2025-12-19 17:10:34 +0700

    update code

--- src/main/java/com/example/travelappapi/config/SecurityConfig.java
+++ src/main/java/com/example/travelappapi/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
             .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
 
             // Khu vực dành riêng cho Quản trị viên
-            .requestMatchers("/api/admin/**").permitAll()
+            .requestMatchers("/api/admin/**").hasRole("Admin")
 
             // Các khu vực còn lại yêu cầu phải đăng nhập
             .anyRequest().authenticated()
