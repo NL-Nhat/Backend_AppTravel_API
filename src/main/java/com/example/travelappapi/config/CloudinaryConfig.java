a83bc35f44d7d132d1baf7a690e2187529858c1a
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2025-12-28 21:25:01 +0700

    update

--- src/main/java/com/example/travelappapi/config/CloudinaryConfig.java
+++ src/main/java/com/example/travelappapi/config/CloudinaryConfig.java
@@ -14,9 +14,9 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dgwjvcvox"); // Thay bằng Cloud Name
-        config.put("api_key", "912938247644598");       // Thay bằng API Key
-        config.put("api_secret", "WW2bAeMyRPNvzALd3G1iAM736FI"); // Thay bằng API Secret
+        config.put("cloud_name", ""); // Thay bằng Cloud Name
+        config.put("api_key", "");       // Thay bằng API Key
+        config.put("api_secret", ""); // Thay bằng API Secret
         config.put("secure", "true");
         return new Cloudinary(config);
     }
